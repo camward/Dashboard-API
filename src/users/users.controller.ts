e4baf43aac646b43879cac4e35071d266ede9f36
alex <camward@yandex.ru>
2022-02-19 21:35:35 +0400

    Dependency Injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import { injectable, inject } from "inversify";
 import { BaseController } from "../common/base.controller";
 import { HTTPError } from "../errors/http-error.class";
-import { LoggerService } from "../logger/logger.service";
+import { ILogger } from "../logger/logger.interface";
+import { TYPES } from "../types";
+import "reflect-metadata";
+import { IUserController } from "./users.controller.interface";
 
-export class UserController extends BaseController {
-  constructor(logger: LoggerService) {
-    super(logger);
+@injectable()
+export class UserController extends BaseController implements IUserController {
+  constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+    super(loggerService);
     this.bindRoutes([
       { path: "/register", method: "post", func: this.register },
       { path: "/login", method: "post", func: this.login },
     ]);
   }
 
-  login(req: Request, res: Response, next: NextFunction) {
+  login(req: Request, res: Response, next: NextFunction): void {
     next(new HTTPError(401, "ошибка авторизации", "login"));
   }
 
-  register(req: Request, res: Response, next: NextFunction) {
+  register(req: Request, res: Response, next: NextFunction): void {
     this.ok(res, "register");
   }
 }
