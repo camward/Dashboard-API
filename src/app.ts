9ab05c535df258d470d96d8d79fa2c9bb7a76a4b
alex <camward@yandex.ru>
2022-02-23 17:32:42 +0400

    JWT-токен. Guard

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Express } from "express";
 import { Server } from "http";
 import { inject, injectable } from "inversify";
-import { ExeptionFilter } from "./errors/exeption.filter";
 import { ILogger } from "./logger/logger.interface";
 import { TYPES } from "./types";
 import { json } from "body-parser";
@@ -10,6 +9,7 @@ import { IConfigService } from "./config/config.service.interface";
 import { IExeptionFilter } from "./errors/exeption.filter.interface";
 import { UserController } from "./users/users.controller";
 import { PrismaService } from "./database/prisma.service";
+import { AuthMiddleware } from "./common/auth.middleware";
 
 @injectable()
 export class App {
@@ -30,6 +30,8 @@ export class App {
 
   useMiddleware(): void {
     this.app.use(json());
+    const authMiddleware = new AuthMiddleware(this.configService.get("SECRET"));
+    this.app.use(authMiddleware.execute.bind(authMiddleware));
   }
 
   useRoutes(): void {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,7 @@ import { ValidateMiddleware } from "../common/validate.middleware";
 import { sign } from "jsonwebtoken";
 import { IConfigService } from "../config/config.service.interface";
 import { IUserService } from "./users.service.interface";
+import { AuthGuard } from "../common/auth.guard";
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -33,6 +34,12 @@ export class UserController extends BaseController implements IUserController {
         method: "post",
         func: this.login,
         middlewares: [new ValidateMiddleware(UserLoginDto)],
+      },
+      {
+        path: "/info",
+        method: "get",
+        func: this.info,
+        middlewares: [new AuthGuard()],
       },
     ]);
   }
@@ -63,6 +70,15 @@ export class UserController extends BaseController implements IUserController {
       return next(new HTTPError(422, "Такой пользователь уже существует"));
     }
     this.ok(res, { email: result.email, id: result.id });
+  }
+
+  async info(
+    { user }: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const userInfo = await this.userService.getUserInfo(user);
+    this.ok(res, { email: userInfo?.email, id: userInfo?.id });
   }
 
   private signJWT(email: string, secret: string): Promise<string> {
